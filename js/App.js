1ca08cc6ff4175ec7e4371ed0d6d20204b80e442
Igor Zoriy <igor.zoriy@gmail.com>
2013-11-25 11:30:34 +0700

    Add animation for App

--- js/App.js
+++ js/App.js
@@ -1,11 +1,19 @@
 "use strict";
 
+var requestAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame;
+var cancelRequestAnimFrame = window.cancelRequestAnimationFrame || window.mozCancelRequestAnimationFrame || window.webkitCancelRequestAnimationFrame || window.msCancelRequestAnimationFrame || window.oCancelRequestAnimationFrame || window.CancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.msCancelAnimationFrame || window.oCancelAnimationFrame;
+
 function App() {
+    this.requestFrameId = null;
+    this.frameDelay = 2000;
+    this.lastFrameTs = 0;
+
     this.snake = new Snake(0, 0, 3);
     this.area = new Area(5, 5);
     this.area.snake = this.snake;
 
     document.body.addEventListener('keydown', this.onKeyDown.bind(this));
+    this.start();
 }
 
 App.prototype.onKeyDown = function (e) {
@@ -23,4 +31,22 @@ App.prototype.onKeyDown = function (e) {
             this.snake.direction = 'down';
             break;
     }
+};
+
+App.prototype.frame = function () {
+    var currentTs = new Date().getTime();
+    if (this.lastFrameTs + this.frameDelay <= currentTs) {
+        this.lastFrameTs = currentTs;
+        this.snake.move();
+        this.area.render();
+    }
+    this.requestFrameId = requestAnimFrame(this.frame.bind(this));
+};
+
+App.prototype.start = function () {
+    this.frame();
+};
+
+App.prototype.stop = function () {
+    cancelRequestAnimFrame(this.requestFrameId);
 };
